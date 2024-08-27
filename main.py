5ab3d476352b7e64f493451d9ba4ccdd5396d11f
Malik <malik.niederhauser@mygymer.ch>
2024-08-27 08:23:51 +0200

    Network Object

--- main.py
+++ main.py
@@ -19,6 +19,8 @@ y = np.eye(10)[y]
 
 class Layer:
     def __init__(self, input_dim, output_dim): # output_dim = number of neurons
+        self.input_dim = input_dim
+        self.output_dim = output_dim
         self.weights = np.array([[random.randint(1, 999)/1000 for _ in range(input_dim)]for _ in range(output_dim)])
         self.weights = np.random.randn(input_dim, output_dim)
         self.bias = np.random.randn(output_dim)
@@ -30,6 +32,30 @@ class Activation:
     def forward(self, input):
         self.output = np.maximum(0, input)
 
+class Network:
+    def __init__(self, *layers: tuple):
+        self.layers = []
+        for layer in layers:
+            self.layers.append(Layer(layer[0], layer[1]))
+        self.activation_object = Activation()
+
+    def add(self, layer):
+        self.layers.append(layer)
+    
+    def print_network(self):
+        for i, layer in enumerate(self.layers):
+            print("Layer {i} takes {layer.input_dim} inputs and has {layer.output_dim} neurons")
+
+    def forward(self, X):
+        for layer in self.layers:
+            X = layer.forward(X)
+            X = self.activation_object.forward(X)
+        return X
+    
+    def backward(self, X, y):
+        pass
+            
+"""
 layer1 = Layer(784, 3) # 784 input features, 3 neurons
 layer2 = Layer(3, 2) # 3 input features, 2 neurons
 layer3 = Layer(2, 10) # 2 input features, 1 neuron
@@ -47,9 +73,8 @@ prediction = activation1.output
 loss = np.sum((prediction - y)**2)
 cross_entropy_loss = -np.sum(y * np.log(prediction + 1e-10))
 
-
-
-
 print("Loss of the network:")
 print("MSE: ", loss)
-print("Cross Entropy: ", cross_entropy_loss)
+print("Cross Entropy: ", cross_entropy_loss)"""
+
+network = Network((784, 3), (3, 2), (2, 10))
